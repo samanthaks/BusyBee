2380ff3177841956e58497911ee099867c9cfd00
Sam Stultz <sks2200@columbia.edu>
2016-10-30 08:24:48 -0400

    non-functioning user login additions

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,10 +1,48 @@
 from app import db
-from werkzeug.security import check_password_hash
+#from werkzeug.security import check_password_hash
+from flask_mongoengine.wtf import model_form
+from flask_mongoengine.wtf.orm import validators
+from wtforms.fields import PasswordField
 
 
 class User(db.Document):
-    def __init__(self, username):
+    #username = db.StringField(required=True, max_length=20, unique=True)
+    email = db.EmailField(required=True)
+    password = PasswordField(required=True, max_length=20)
+    active = db.BooleanField(default=True)
+
+    def get_by_email_w_password(self, emails):
+
+        try:
+            print "hello"
+            dbUser = self.objects.get(email=emails)
+            
+            if dbUser:
+                print "dbUser exists"
+                email = dbUser.email
+                #self.active = dbUser.active
+                password = dbUser.password
+                #self.id = dbUser.id
+                return self
+            else:
+                return None
+        except:
+            print "there was an error"
+            return None
+
+    
+
+    def __init__(self, email=None, password=None, active=True):
+        email = email
+        password = password
+        active = active
+        #self.isAdmin = False
+        #self.id = None
+
+    """    def __init__(self, username, password, email):
         self.username = username
+        self.password = password
+        self.email = email
 
     def is_authenticated(self):
         return True
@@ -21,3 +59,16 @@ class User(db.Document):
     @staticmethod
     def validate_login(password_hash, password):
         return check_password_hash(password_hash, password)
+"""
+
+user_form = model_form(User, exclude=['password'])
+
+
+class SignupForm(user_form):
+    password = PasswordField('Password', validators=[validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+
+
+# Login form will provide a Password field (WTForm form field)
+class LoginForm(user_form):
+    password = PasswordField('Password', validators=[validators.Required()])
--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,12 +1,15 @@
-from app import lm
-from flask import Blueprint, request, redirect, render_template, url_for, flash
+from flask import Blueprint, request, redirect, render_template, url_for, flash, current_app
+from app import lm, flask_bcrypt
 from flask_login import login_user, logout_user, login_required
-from app.models.login_form_model import LoginForm
-from app.models.user_model import User
+#from app.models.login_form_model import LoginForm
+
+#from app.User import UserSub
+from app.models.user_model import User, SignupForm  #, LoginForm
+
 
 user = Blueprint('users', __name__, template_folder='../templates')
 
-
+"""
 @user.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -45,3 +48,92 @@ def load_user(username):
     if not u:
         return None
     return User(u['_id'])
+"""
+
+
+@user.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST" and "email" in request.form:
+        email = request.form["email"]
+        userObj = User()
+        user = userObj.get_by_email_w_password(email)
+        if user and flask_bcrypt.check_password_hash(user.password, request.form["password"]) and user.is_active():
+            remember = request.form.get("remember", "no") == "yes"
+
+            if login_user(user, remember=remember):
+                flash("Logged in!")
+                return redirect('/')
+            else:
+                flash("unable to log you in")
+
+    return render_template("/login.html")
+
+
+@user.route("/register", methods=["GET", "POST"])
+def register():
+    
+    registerForm = SignupForm(request.form)
+    current_app.logger.info(request.form)
+
+    if request.method == 'POST' and registerForm.validate() == False:
+        current_app.logger.info(registerForm.errors)
+        return "registration error"
+
+    elif request.method == 'POST' and registerForm.validate():
+        email = request.form['email']
+
+        # generate password hash
+        password_hash = flask_bcrypt.generate_password_hash(request.form['password'])
+
+        # prepare User
+        user = User(email=email, password=password_hash)
+        print user
+        print email
+        print password_hash
+
+        try:
+            user.save()
+            print "saved"
+            if login_user(user, remember="no"):
+                flash("Logged in!")
+                return redirect('/')
+            else:
+                flash("unable to log you in")
+
+        except:
+            flash("unable to register with that email address")
+            current_app.logger.error("Error on registration - possible duplicate emails")
+
+    # prepare registration form         
+    registerForm = SignupForm(csrf_enabled=True)
+    templateData = {
+
+        'form' : registerForm
+    }
+
+    return render_template("/register.html", **templateData)
+
+
+@user.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out.")
+    return redirect('/login')
+
+
+@lm.unauthorized_handler
+def unauthorized_callback():
+    return redirect('/login')
+
+
+@lm.user_loader
+def load_user(id):
+    if id is None:
+        redirect('/login')
+    user = User()
+    user.get_by_id(id)
+    if user.is_active():
+        return user
+    else:
+        return None
