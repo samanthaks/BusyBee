1f8ad66a16538bbe8070f4b4acfb1203cbcbe7ad
roch09 <rsj2115@columbia.edu>
2016-10-29 20:23:27 -0400

    app updated

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from app.models.request import Request
+from flask import Blueprint, render_template, redirect, url_for, request 
+from app.models.request import Request, RequestForm
 
 
 tasks = Blueprint('tasks', __name__, template_folder='../templates')
@@ -10,3 +10,15 @@ def tasks_page():
     """The main tasks page"""
     request = Request.objects()
     return render_template('tasks.html', tasks=request)
+
+
+@tasks.route('/new', methods=['GET', 'POST'])
+def new():
+    """Create a new post"""
+    form = RequestForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        form.save()
+        return redirect(url_for('tasks.tasks_page'))
+
+    return render_template('new.html', form=form)
