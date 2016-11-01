a8c4952ca5685fccb5ee1b2e515d12aaabf65367
Sam Stultz <sks2200@columbia.edu>
2016-11-01 14:06:44 -0400

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import local
+
+
+def first_setup():
+    local("pip install virtualenv")
+    local("virtualenv .")
+    local("source bin/activate")
+    local("pip install -r requirements.txt")
+    local("deactivate")
+
+
+def run_local():
+    local("python run.py")
+
+
+def update_req():
+    local("pip freeze > requirements.txt")
+
+
+def enter_venv():
+    local("source bin/activate")
